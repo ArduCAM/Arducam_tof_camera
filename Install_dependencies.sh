7b5f5f702ca8957ebedc9977b05602d44dc214d8
Dion4cen <132877624+Dion4cen@users.noreply.github.com>
2024-04-08 14:26:34 +0800

    Update Install_dependencies.sh *3

--- Install_dependencies.sh
+++ Install_dependencies.sh
@@ -8,8 +8,8 @@ if [ -f "/boot/firmware/config.txt" ]; then  # Bookworm
     fi
 elif [ -f "/boot/config.txt" ]; then         # Bullseye and earlier
     FIND_FILE="/boot/config.txt"
-    if [ $(grep -c "dtoverlay=arducam-pivariety,media-controller=0" $FIND_FILE) -lt '1' ];then
-        echo "dtoverlay=arducam-pivariety,media-controller=0" | sudo tee -a $FIND_FILE
+    if [ $(grep -c "dtoverlay=arducam-pivariety" $FIND_FILE) -lt '1' ];then
+        echo "dtoverlay=arducam-pivariety" | sudo tee -a $FIND_FILE
     fi
 fi
 

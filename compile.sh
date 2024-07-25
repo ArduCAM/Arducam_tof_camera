a85724e814b28ee0cd1668b3a813cbf3dcdab22b
antonio-ardu <Antonio@arducam.com>
2024-07-25 10:04:29 +0800

    Add more build target

--- compile.sh
+++ compile.sh
@@ -5,5 +5,8 @@ workpath=$(cd "$(dirname "$0")" && pwd)
 echo "workpath: $workpath"
 cmake -B $workpath/build -S $workpath \
 && cmake --build $workpath/build --config Release --target example -j 4 \
+&& cmake --build $workpath/build --config Release --target preview_depth_c -j 4 \
+&& cmake --build $workpath/build --config Release --target preview_depth -j 4 \
+&& cmake --build $workpath/build --config Release --target capture_raw -j 4 \
 && echo "== Run $workpath/build/example/cpp/example" \
 || echo "== Build failed"

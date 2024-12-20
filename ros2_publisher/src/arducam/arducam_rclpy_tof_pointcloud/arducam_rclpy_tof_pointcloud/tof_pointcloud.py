32249d6d63ec03ad69497e701f4bc3402f44fe1e
antonio-ardu <Antonio@arducam.com>
2024-12-20 09:26:12 +0800

    update readme and ros for python

--- ros2_publisher/src/arducam/arducam_rclpy_tof_pointcloud/arducam_rclpy_tof_pointcloud/tof_pointcloud.py
+++ ros2_publisher/src/arducam/arducam_rclpy_tof_pointcloud/arducam_rclpy_tof_pointcloud/tof_pointcloud.py
@@ -1,4 +1,5 @@
-from math import tan, pi
+from argparse import ArgumentParser
+from typing import Optional
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import PointCloud2
@@ -10,7 +11,6 @@ from threading import Thread
 
 from ArducamDepthCamera import (
     ArducamCamera,
-    CameraInfo,
     Connection,
     DeviceType,
     FrameType,
@@ -18,40 +18,77 @@ from ArducamDepthCamera import (
     DepthData,
 )
 
+class Option:
+    cfg: Optional[str]
+    
+
 
 class TOFPublisher(Node):
-    def __init__(self, tof: ArducamCamera, camera_info: CameraInfo):
+    def __init__(self, options: Option):
         super().__init__("arducam")
+
+        tof = self.__init_camera(options)
+        if tof is None:
+            raise Exception("Failed to initialize camera")
+
         self.tof_ = tof
-        if camera_info.device_type == TOFDeviceType.HQVGA:
-            self.width_ = camera_info.width
-            self.height_ = camera_info.height
-        else:
-            self.width_ = camera_info.width
-            self.height_ = camera_info.height // 10 - 1
         self.pointsize_ = self.width_ * self.height_
         self.frame_id = "sensor_frame"
         self.depth_msg_ = Float32MultiArray()
-        self.timer_ = self.create_timer(1 / 30, self.update)
         self.publisher_ = self.create_publisher(PointCloud2, "point_cloud", 10)
         self.publisher_depth_ = self.create_publisher(
             Float32MultiArray, "depth_frame", 10
         )
-        self.fx = self.width_ / (2 * tan(0.5 * pi * 64.3 / 180))
-        self.fy = self.height_ / (2 * tan(0.5 * pi * 50.4 / 180))
+        self.fx = tof.getControl(Control.INTRINSIC_FX) / 100
+        self.fy = tof.getControl(Control.INTRINSIC_FY) / 100
         self.header = Header()
         self.header.frame_id = "map"
         self.points = None
         self.running_ = True
+        self.timer_ = self.create_timer(1 / 30, self.update)
         self.process_point_cloud_thr = Thread(
             target=self.__generateSensorPointCloud, daemon=True
         )
         self.process_point_cloud_thr.start()
 
+    def __init_camera(self, options: Option):
+        print("pointcloud publisher init")
+        tof = ArducamCamera()
+        ret = 0
+        
+        if options.cfg is not None:
+            ret = tof.openWithFile(options.cfg, 0)
+        else:
+            ret = tof.open(Connection.CSI, 0)
+        if ret != 0:
+            print("Failed to open camera. Error code:", ret)
+            return
+
+        ret = tof.start(FrameType.DEPTH)
+        if ret != 0:
+            print("Failed to start camera. Error code:", ret)
+            tof.close()
+            return
+
+        info = tof.getCameraInfo()
+        if info.device_type == DeviceType.HQVGA:
+            self.width_ = info.width
+            self.height_ = info.height
+            tof.setControl(Control.RANGE, 4)
+        elif info.device_type == DeviceType.VGA:
+            self.width_ = info.width
+            self.height_ = info.height // 10 - 1
+        print(f"Open camera success, width: {self.width_}, height: {self.height_}")
+
+        print("Pointcloud publisher start")
+        return tof
+
     def __generateSensorPointCloud(self):
         while self.running_:
             frame = self.tof_.requestFrame(200)
             if frame is not None and isinstance(frame, DepthData):
+                self.fx = self.tof_.getControl(Control.INTRINSIC_FX) / 100
+                self.fy = self.tof_.getControl(Control.INTRINSIC_FY) / 100
                 depth_buf = frame.depth_data
                 confidence_buf = frame.confidence_data
 
@@ -94,37 +131,25 @@ class TOFPublisher(Node):
     def stop(self):
         self.running_ = False
         self.process_point_cloud_thr.join()
+        self.tof_.stop()
+        self.tof_.close()
 
 
 def main(args=None):
     rclpy.init(args=args)
-    tof = ArducamCamera()
-
-    ret = 0
-    ret = tof.open(Connection.CSI, 0)
-    if not ret:
-        print("Failed to open camera. Error code:", ret)
-        return
-
-    ret = tof.start(FrameType.DEPTH)
-    if ret != 0:
-        print("Failed to start camera. Error code:", ret)
-        tof.close()
-        return
-
-    info = tof.getCameraInfo()
-    if info.device_type == DeviceType.HQVGA:
-        tof.setControl(Control.RANGE, 4)
-
-    print("pointcloud publisher start")
-
-    tof_publisher = TOFPublisher(tof, info)
+    parser = ArgumentParser()
+    parser.add_argument("--cfg", type=str, help="Path to camera configuration file")
+    
+    ns = parser.parse_args()
+    
+    options = Option()
+    options.cfg = ns.cfg
+    
+    tof_publisher = TOFPublisher(options)
 
     rclpy.spin(tof_publisher)
     rclpy.shutdown()
     tof_publisher.stop()
-    tof.stop()
-    tof.close()
 
 
 if __name__ == "__main__":
